c77c4b689e9c029936f01e946da1fb220c0234c6
Derrick Rono <derrickrono@gmail.com>
2016-02-15 17:46:11 +0300

    Added Red bean ORM to save data to the MariaDB

--- lib/AirtelMoney.php
+++ lib/AirtelMoney.php
@@ -1,4 +1,5 @@
 <?php
+require_once('rb.php');
 ini_set("soap.wsdl_cache_enabled", "0");
 /**
  * Airtel API powerengine
@@ -8,14 +9,17 @@ ini_set("soap.wsdl_cache_enabled", "0");
  * This Class to allow access to Airtel money merchant queries
  */
 
-
 class AirtelMoney
 {
 
 	public function processMerchantQuery($username,$password,$REFERENCE_ID,$MSISDN,$request_type){
-		
+	/**
+	 * Initialize redbean library
+	 */
+	//To connect to real db 
+	
+
 		$params = array(			
-			//'processCheckOutRequest'=>array(
 	            'userName'=>$username,
 	            'passWord'=>$password,
 	            'referenceId'=>$REFERENCE_ID,
@@ -25,13 +29,10 @@ class AirtelMoney
 	          	 */
 	           	'timeFrom'=>'20160115050700', 
 	            'timeTo'=>'20160115110700',
-			//	)
             
             );
-		//For Debug purposes. Remove in production
-		
-		print_r('Requesting....');
-		$soap = new SOAPClient(URL,array("trace"  => 0, "exceptions" => 0,
+		try {
+			$soap = new SOAPClient(dirname(__FILE__)."/MERCHANT_QUERY.wsdl",array("trace"  => 0, "exceptions" => 1,
 			"stream_context" => stream_context_create(
 			        array(
 			            'ssl' => array(
@@ -43,20 +44,71 @@ class AirtelMoney
     			),'location'=>URL,'soap_version'=> SOAP_1_2,
 			'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP
 			));
-		
 
-	      $response=$soap->__call($request_type,array($params));
-		//For Debug purposes. Remove in production
-		  print_r( $this->processResponse($response)."\n");
-		
+	     $response=$soap->__call($request_type,array($params));
+	    
+	     $amount=$response->RequestTransactionResult->Amount;
+	     $firstname=$response->RequestTransactionResult->FirstName;
+	     $lastname=$response->RequestTransactionResult->LastName;
+	     $message=$response->RequestTransactionResult->Message;
+	     $msisdn=$response->RequestTransactionResult->Msisdn;
+	     $referenceId=$response->RequestTransactionResult->Referenceld;
+	     $status=$response->RequestTransactionResult->Status;
+	     $transactionId=$response->RequestTransactionResult->TransactionId;
+
+	     $transaction = array(
+	     	'amount' =>$amount , 
+	     	'firstname' => $firstname, 
+	     	'lastname' => $lastname, 
+	     	'message' => $message, 
+	     	'msisdn' => $msisdn, 
+	     	'referenceId' => $referenceId, 
+	     	'status' => $status, 
+	     	'transactionId' => $transactionId, 
+	     	);
+
+	     
+	     if($transaction["amount"]!="" || $transaction["amount"] !=null){
+	     	//Insert values to the database of choice
+	     	
+	     	/**
+	     	 * Db connection setup with RB ORM
+	     	 */
+	     	//Create a database name called m_payment_transaction and redbean will handle the rest
+	     	//Also make sure you change the username and password
+	     	R::setup( 'mysql:host=localhost;dbname=m_payment_transaction',
+       		 'username', 'password' ); 
+
+	     	/**
+	     	 * Store the data received back from the API
+	     	 * @var [type]
+	     	 */
+       		$trans=R::dispense('airtel');
+       		$trans->amount=$transaction["amount"];			
+       		$trans->firstname=$transaction["firstname"];			
+       		$trans->lastname=$transaction["lastname"];			
+       		$trans->message=$transaction["message"];			
+       		$trans->msisdn=$transaction["msisdn"];			
+       		$trans->referenceId=$transaction["referenceId"];			
+       		$trans->status=$transaction["status"];			
+       		$trans->transactionId=$transaction["transactionId"];			
+			$trans=R::store( $trans );
+
+	     	var_dump($trans);
+	     }
+	     else{
+	     	echo "no data received";
+	     	$response=$soap->__call($request_type,array($params));
+	     	
+	     }
+	     
+	    	
+
+		} catch (Exception $e) {
+			echo $e;
+		}	
 
 	}
-
-	function processResponse($responseData){
-
-		return json_encode($responseData);
-	}
-
 	
 	
 }
